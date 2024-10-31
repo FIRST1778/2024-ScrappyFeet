3c82e96453d29120f78ba875e0f9968fba069a82
Simon Branch <simonmbranch@gmail.com>
2024-10-30 17:42:48 -0700

    Fix indentation, remove spaces before colons. code prettification

--- src/main/java/org/chillout1778/subsystems/SwerveModule.kt
+++ src/main/java/org/chillout1778/subsystems/SwerveModule.kt
@@ -14,9 +14,10 @@ class SwerveModule(
     val driveInverted: InvertedValue,
     val turnInverted: InvertedValue
 ) {
-    val driveMotor : TalonFX
-    val turnMotor : TalonFX
-    val canCoder : CANcoder
+    val driveMotor: TalonFX
+    val turnMotor: TalonFX
+    val canCoder: CANcoder
+
     init {
         driveMotor = TalonFX(driveMotorID)
         turnMotor = TalonFX(turnMotorID)
@@ -32,15 +33,17 @@ class SwerveModule(
             MotorOutputConfigs().withInverted(driveInverted),
         )
         turnMotor.configurator.apply(
-                MotorOutputConfigs().withInverted(turnInverted)
-                )
+            MotorOutputConfigs().withInverted(turnInverted)
+        )
     }
+
     val turnPID = Constants.Swerve.makeTurnPID()
 
-    fun turnPosition() : Double {
+    fun turnPosition(): Double {
         return turnMotor.position.valueAsDouble * 2 * Math.PI
     }
-    fun drive(angle : Double, driveVelocity : Double) {
+
+    fun drive(angle: Double, driveVelocity: Double) {
         val turnVoltage = turnPID.calculate(turnPosition(), angle)
         turnMotor.setVoltage(turnVoltage)
         driveMotor.setVoltage(driveVelocity * Constants.Swerve.MAX_SPEED / 12)
