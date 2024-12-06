29449623a0e5175cc219bce0577d1a7bde3ec020
Maya A <mynameismaya2430@gmail.com>
2024-12-05 19:50:17 -0800

    Co-Routine Practice

--- src/main/java/org/chillout1778/subsystems/SwerveModule.kt
+++ src/main/java/org/chillout1778/subsystems/SwerveModule.kt
@@ -113,7 +113,7 @@ class SwerveModule(
 
     override fun initSendable(builder: SendableBuilder?) {
         builder!!
-
+        builder.clearProperties()
         builder.addDoubleProperty("raw turn position", {turnMotor.position.valueAsDouble}, {})
         builder.addDoubleProperty("raw cancoder position", {canCoder.absolutePosition.valueAsDouble}, {})
 
